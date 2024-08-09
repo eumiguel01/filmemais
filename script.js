e66ce14673c153d51a6f4551f1e4fbe7ad524b5a
Miguel da Silva Xavier <143849967+eumiguel01@users.noreply.github.com>
2024-08-09 12:30:49 -0300

    Criando a aba de adicionar

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
+let secaoAdicionarFilme = document.querySelector("#secaoAdicionarFilme")
+let cabecalho = document.querySelector("#header")
+let principal = document.querySelector("#main")
 const adicionarFilme = document.querySelector("#criar")
 adicionarFilme.addEventListener("click", () => {
-    
+    secaoAdicionarFilme.style.display = "block"
+    cabecalho.style.filter =  "blur(2px)"
+    principal.style.filter =  "blur(2px)"
 })
